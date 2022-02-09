247a05d79be9dbc64c17a129144bc9ece01328d3
Wagner Morais <wagner.morais@objective.com.br>
2022-02-09 17:41:14 -0300

    implementing simple components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
+import { HelloComponentComponent } from './hello-component/hello-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    HelloComponentComponent
   ],
   imports: [
     BrowserModule
